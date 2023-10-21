d514a3e52757268367685d7094aee62048d717a2
rmiyoyo <miyoyoraphael@gmail.com>
2023-10-22 01:05:56 +0300

    Improve homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchTownInfo } from './redux/home/homeSlice';
 import Layout from './components/Layout';
 import Home from './routes/Home';
 import Info from './routes/Info';
 import PageError from './routes/PageError';
 import './App.css';
+import './styles/home.scss';
+import './styles/nav.scss';
+import './styles/info.scss';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTownInfo());
+  }, [dispatch]);
   return (
     <div className="App">
       <Routes>
