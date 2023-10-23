d302b474997b26167499cace82c7c983a2c89ed8
rmiyoyo <miyoyoraphael@gmail.com>
2023-10-23 16:14:19 +0300

    Update packages

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 // jest.config.js
 module.exports = {
-    transform: {
-        "^.+\\.(js|jsx)$": "babel-jest"
-    },
-    // Other Jest configuration options...
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  // Other Jest configuration options...
 };
--- src/tests/Townlist.test.jsx
+++ src/tests/Townlist.test.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import { Provider } from 'react-redux'; // If you are using Redux
-import TownList from '../components/Townlist'; // Import your component
-import store from '.redux'; // Import your Redux store
+import { Provider } from 'react-redux';
+import TownList from '../components/Townlist';
+import store from './.redux';
 
-// Sample test data
 const sampleTowns = [
   {
     city: 'City1',
@@ -45,8 +44,6 @@ describe('TownList Component', () => {
       </Provider>,
     );
 
-    // Assuming 'fetchingData' and 'error' are both false
-    // You should see the city names in the rendered component
     sampleTowns.forEach((town) => {
       const cityName = getByText(town.city);
       expect(cityName).toBeInTheDocument();
@@ -66,13 +63,11 @@ describe('TownList Component', () => {
       </Provider>,
     );
 
-    // Assuming 'fetchingData' and 'error' are both false
     const cityItem = sampleTowns[0];
     const cityName = getByText(cityItem.city);
 
     fireEvent.click(cityName);
 
-    // Ensure that the dispatch function was called with the correct cityItem
     expect(mockDispatch).toHaveBeenCalledWith(addActiveTown(cityItem));
   });
 });
